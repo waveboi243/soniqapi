afafd5c70ab80186ec8261d83ed7827602eaeb2c
waveboi243 <leon032020@gmail.com>
2024-09-29 21:15:19 -0400

    middleware fixing

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8100"],
+    allow_origins=["*"],
     allow_methods = ["*"],
     allow_headers = ["*"]
 )
